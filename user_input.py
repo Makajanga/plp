925c4d7aa663bbfd903698cd1fc00474287a93fd
USER <daudmakajanga2901@gmail.com>
2024-02-28 14:27:00 +0300

    Initial commit

--- user_input.py
+++ user_input.py
@@ -1,7 +1,7 @@
 # Ask user's name
 name = ('what is your name?')
 # Ask user's age
-Age = ('what is your age?')
+age = ('what is your age?')
 # Ask user's location
 location = ("where do you live?")
 print("Hello {Daud} you are{23}years old and live in {Dar es salaam}")
