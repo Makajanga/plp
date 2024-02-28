925c186eac12adee3a66d63758187396c4b41304
USER <daudmakajanga2901@gmail.com>
2024-02-28 14:21:07 +0300

    Initial commit

---
+++ Daudizo.py
@@ -0,0 +1,57 @@
+#name_of_a_person
+first_name = 'daud'
+last_name = 'makajanga'
+print(first_name)
+print(first_name, last_name)
+Age = 30
+work = 'shopkeeper'
+print(Age, work)
+id(Age)
+type(Age)
+print(type)
+x = 30*30
+print(x)
+type(x)
+Age_respondent = [45,67,23,21]
+print(Age_respondent)
+print(Age_respondent[0])
+print(Age_respondent[1])
+print(Age_respondent[2])
+Age_respondent[3]= 67
+print(Age_respondent[3])
+print(Age_respondent)
+x= 30
+y=40
+results0= (x+y)
+results= (x-y)
+print(results0)
+print(results)
+results2= (x/y)
+print(results2)
+results3= (y/2*x)
+print(results3)
+results4= ((x/y)*23)
+print(results4)
+var = 'daudizo'
+print(var[0:4])
+print(var[0:5])
+print(var[0:14])
+len(var)
+var1 = "it is a great day"
+var2 = "enjoy sunday "
+var3 = var1+var2 #it is a sunday enjoy
+var4 = var1[0:6]+var2[6:14]+var2[0:5]
+print(var4)
+x= 10
+print("x is of type:",type(x))
+y=13.4
+print("y is of type:",type(y))
+z=x+y
+print(z)
+print("z is a type:", type(z))
+print(x,0)
+s= 1010
+c= int(s,2)
+print("After converting character to integer : ",end="")
+print(c)
+
